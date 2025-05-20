66d86659a3ccae986575178cd3162d1043c68a9e
DREAM <phonedownload500@gmail.com>
2025-05-20 18:09:43 +0530

    admin account

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const signupRoute = require("./routes/signup");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const createAdminAccount = require("./scripts/admin");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
+
+createAdminAccount();
+
 app.use("/user", signupRoute);
 
 // Connect Server
