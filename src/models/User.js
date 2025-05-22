56109f9fedd55d5625e14157af5387df55c392ff
PriyanshuDeveloper26 <127717374+PriyanshuDeveloper26@users.noreply.github.com>
2025-05-22 13:57:10 +0530

    auth

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,10 @@
 const mongoose = require("../configuration/dbConfig");
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  role: {
-    type: String,
-    enum: ["admin", "user"],
-    default: "user",
-  },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["admin", "user"], default: "user" },
 });
 
 module.exports = mongoose.model("User", UserSchema);
--- src/services/login.js
+++ src/services/login.js
@@ -3,23 +3,26 @@ const User = require("../models/User");
 const { generateToken } = require("../utils/jwtUtils");
 
 async function login(email, password) {
-    try {
-       const existingUser = await User.findOne({email});
-       if(!existingUser){
-        throw new Error("User not found");
-       }
-       const isPasswordValid = await bcrypt.compare(password, existingUser.password);
-       if(!isPasswordValid){
-        throw new Error("Invalid password");
-       }
-       const token = generateToken(existingUser);
-       return token;
-    } catch (error) {
-        console.log("login error", error.message);
-        throw new Error("Invalid credentials");
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new Error("User not found");
     }
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!isPasswordValid) {
+      throw new Error("Invalid password");
+    }
+    const token = generateToken(existingUser);
+    return token;
+  } catch (error) {
+    console.log("login error", error.message);
+    throw new Error("Internal server error");
+  }
 }
 
 module.exports = {
-    login
-}
+  login,
+};
