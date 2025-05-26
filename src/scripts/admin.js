c66b779cd6a65c2ed4a021a77412e5de22011016
PriyanshuDeveloper26 <127717374+PriyanshuDeveloper26@users.noreply.github.com>
2025-05-26 19:14:41 +0530

    alert box in admin.js

--- src/scripts/admin.js
+++ src/scripts/admin.js
@@ -3,23 +3,26 @@ const bcrypt = require("bcrypt");
 
 // Create Admin Account
 async function createAdminAccount() {
-    try {
-       const existingAdmin = await User.findOne({email:"zidiodev@gmail.com"});
-       if (!existingAdmin){
-          const newAdmin = new User({
-            name:"Admin",
-            email:"zidiodev@gmail.com",
-            password: await bcrypt.hash("admin@123",10),
-            role:"admin"
-           });
-           await newAdmin.save();
-           console.log("Admin account created successfully");
-       }else{
-        console.log("Admin already exist");
-       }
-    } catch (error) {
-        console.error(error.message);
+  try {
+    const existingAdmin = await User.findOne({ email: "zidiodev@gmail.com" });
+    if (!existingAdmin) {
+      const newAdmin = new User({
+        name: "Admin",
+        email: "zidiodev@gmail.com",
+        password: await bcrypt.hash("admin@123", 10),
+        role: "admin",
+      });
+      await newAdmin.save();
+      console.log("Admin account created successfully");
+      alert("Admin account created successfully");
+    } else {
+      console.log("Admin already exist");
+      alert("Admin already exist");
     }
+  } catch (error) {
+    console.error(error.message);
+    alert(error.message);
+  }
 }
 
-module.exports = createAdminAccount; 
+module.exports = createAdminAccount;
