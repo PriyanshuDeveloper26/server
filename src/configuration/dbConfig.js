b31a3527178342169f191daf5a9f4a5cb809b296
PriyanshuDeveloper26 <127717374+PriyanshuDeveloper26@users.noreply.github.com>
2025-05-26 13:14:55 +0530

    mongouri change

--- src/configuration/dbConfig.js
+++ src/configuration/dbConfig.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-"mongodb+srv://sathavarapriyanshu9:sathavarapriyanshu9@cluster0.sufnxqy.mongodb.net/excel_analytics_platform?retryWrites=true&w=majority&appName=Cluster0"
-);
+// for atlas
+// mongoose.connect(
+// "mongodb+srv://sathavarapriyanshu9:sathavarapriyanshu9@cluster0.sufnxqy.mongodb.net/excel_analytics_platform?retryWrites=true&w=majority&appName=Cluster0"
+// );
 
 // for mongodb compass
-// mongoose.connect(
-//   "mongodb://localhost:27017/excel_analytics_platform"
-// );
+mongoose.connect(
+  "mongodb://localhost:27017/excel_analytics_platform"
+);
 
 mongoose.connection.on("connected", () => {
   console.log("MongoDB connected");
