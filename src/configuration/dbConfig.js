fdec4d0b7f37144e98ece455c6e1e9772efe8bb9
PriyanshuDeveloper26 <127717374+PriyanshuDeveloper26@users.noreply.github.com>
2025-05-28 17:30:44 +0530

    validation for existing file and extension of file......and database configuration for atlas

--- src/configuration/dbConfig.js
+++ src/configuration/dbConfig.js
@@ -1,21 +1,30 @@
 const mongoose = require("mongoose");
 
+const mongoURI = "mongodb+srv://sathavarapriyanshu9:sathavarapriyanshu9@cluster0.sufnxqy.mongodb.net/excel_analytics_platform?retryWrites=true&w=majority&appName=Cluster0";
+
 // for atlas
 // mongoose.connect(
 // "mongodb+srv://sathavarapriyanshu9:sathavarapriyanshu9@cluster0.sufnxqy.mongodb.net/excel_analytics_platform?retryWrites=true&w=majority&appName=Cluster0"
 // );
 
 // for mongodb compass
-mongoose.connect(
-  "mongodb://localhost:27017/excel_analytics_platform"
-);
+// mongoose.connect(
+//   "mongodb://localhost:27017/excel_analytics_platform"
+// );
 
-mongoose.connection.on("connected", () => {
-  console.log("MongoDB connected");
-});
+// mongoose.connection.on("connected", () => {
+//   console.log("MongoDB connected");
+// });
 
-mongoose.connection.on("error", (err) => {
-  console.log("MongoDB connection error:", `${err}`);
-});
+// mongoose.connection.on("error", (err) => {
+//   console.log("MongoDB connection error:", `${err}`);
+// });
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch((err) => console.error("❌ MongoDB connection error:", err));
 
 module.exports = mongoose;
--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -2,36 +2,63 @@ const express = require("express");
 const multer = require("multer");
 const xlsx = require("xlsx");
 const createDynamicModel = require("../models/Record");
-
+const mongoose = require("../configuration/dbConfig");
 const router = express.Router();
 
 // multer storage
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({
+    storage: multer.memoryStorage(),
+    fileFilter: function (req, file, cb) {
+      const allowedTypes = [
+        'application/vnd.ms-excel', // .xls
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' // .xlsx
+      ];
+      if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true);
+      } else {
+        cb(new Error('Only .xls and .xlsx files are allowed!'));
+      }
+    }
+  });
 
 router.post("/upload", upload.single("file"), async (req, res) => {
-    try {
-        const workbook = xlsx.read(req.file.buffer, { type: "buffer" });
-        const sheetName = workbook.SheetNames[0];
-        const jsonData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
-        console.log(jsonData);
+  try {
+    const workbook = xlsx.read(req.file.buffer, { type: "buffer" });
+    const sheetName = workbook.SheetNames[0];
+    const jsonData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+    console.log(jsonData);
 
-        // get headers
-        const headers = Object.keys(jsonData[0]);
-        console.log(headers);
+    // get headers
+    const headers = Object.keys(jsonData[0]);
+    console.log(headers);
 
-        // get file name
-        const fileName = req.file.originalname.split(".")[0].toLowerCase();
-        console.log(fileName);
-        const DynamicModel = createDynamicModel(fileName, headers);
+    // get file name
+    const fileName = req.file.originalname.split(".")[0].toLowerCase();
+    console.log(fileName);
 
-        // insert data into database
-        await DynamicModel.insertMany(jsonData);
-        res.json({ message: `Data from '${fileName}' saved successfully`, count: jsonData.length });
-    } catch (error) {
-        console.error("Excel upload error", error);
-        res.status(500).json({ message: "Failed to process file"});
+    // check if file already exists
+    const existingCollection = await mongoose.connection.db
+      .listCollections({ name: fileName })
+      .toArray();
+    if (existingCollection.length > 0) {
+      return res
+        .status(400)
+        .json({ message: ` '${fileName}' has already been uploaded` });
     }
-})
+
+    // create dynamic model
+    const DynamicModel = createDynamicModel(fileName, headers);
+
+    // insert data into database
+    await DynamicModel.insertMany(jsonData);
+    res.json({
+      message: `Data from '${fileName}' saved successfully`,
+      count: jsonData.length,
+    });
+  } catch (error) {
+    console.error("Excel upload error", error);
+    res.status(500).json({ message: "Failed to process file" });
+  }
+});
 
 module.exports = router;
